4a20d9727b3248466f73deae28eef47dea9cae20
example name <974632489@qq.com>
2018-03-21 13:48:30 +0800

    2018年3月21日  13点48分: 	ssh框架搭建完成，使用maven来构建项目

---
+++ src/main/java/com/bjut/s14024205/entity/PlayBack.java
@@ -0,0 +1,43 @@
+package com.bjut.s14024205.entity;
+
+import java.util.Date;
+
+public class PlayBack {
+
+	int id;
+	String uName;
+	Date liveTime;
+	String fileName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getuName() {
+		return uName;
+	}
+
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+
+	public Date getLiveTime() {
+		return liveTime;
+	}
+
+	public void setLiveTime(Date liveTime) {
+		this.liveTime = liveTime;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
---
+++ src/main/java/com/bjut/s14024205/entity/User.java
@@ -0,0 +1,32 @@
+package com.bjut.s14024205.entity;
+
+public class User {
+	int id;
+	String uName;
+	String passWorld;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getuName() {
+		return uName;
+	}
+
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+
+	public String getPassWorld() {
+		return passWorld;
+	}
+
+	public void setPassWorld(String passWorld) {
+		this.passWorld = passWorld;
+	}
+
+}
