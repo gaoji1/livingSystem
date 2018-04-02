370dada8c89ea40357113ab4cc28b7d7f1bf4023
example name <974632489@qq.com>
2018-04-02 09:01:26 +0800

    2018年4月2日  09点01分： 	注册流程走通

--- src/main/java/com/bjut/s14024205/action/SignUpAction.java
+++ src/main/java/com/bjut/s14024205/action/SignUpAction.java
@@ -1,5 +1,6 @@
 package com.bjut.s14024205.action;
 
+import com.bjut.s14024205.dao.impl.UserDaoImpl;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class SignUpAction extends ActionSupport {
@@ -9,7 +10,14 @@ public class SignUpAction extends ActionSupport {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
+	//注入userdaoimpl
+	private UserDaoImpl u;
+	public UserDaoImpl getU() {
+		return u;
+	}
+	public void setU(UserDaoImpl u) {
+		this.u = u;
+	}
 	//验证注册函数(测试)
 	public void verify() {
 		System.out.println("成功进入");
--- src/main/webapp/js/SignUp.js
+++ src/main/webapp/js/SignUp.js
@@ -3,7 +3,23 @@
  */
 
 
+var func = function(){
+	var username =$("#username").val();
+	var password = $("#password").val();
+	var url = "/livingSystem/SignUpAction_verify";
+	$.post(url,
+			{uName : username,
+			passWord : password},
+			function(data){
+				if(data == "success"){
+					$(location).attr('href', '/livingSystem/LogIn.html');
+				}else if(data == "wrong password"){
+					alert(data);
+				}else if(data == "username does not exist"){
+					alert(data);
+				}
+			})
+}
 
-$(function(){
-	alert(1);
-})
+
+$("#submit").on("click",func);
