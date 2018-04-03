2973e75f712d79a1e428294d84fdcfa9b3a86b6e
example name <974632489@qq.com>
2018-04-03 16:41:44 +0800

    2018年4月3日   16点41分 	完善用户管理界面 	现在可以查询当前用户的直播信息 	由服务端转化成json传递给浏览器，浏览器解析后 	输出到对应的标签栏
    	服务端不再等待浏览器传递用户名，自行从session中取用户名

--- src/main/java/com/bjut/s14024205/action/UserManage.java
+++ src/main/java/com/bjut/s14024205/action/UserManage.java
@@ -1,11 +1,27 @@
 package com.bjut.s14024205.action;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.alibaba.fastjson.JSON;
+import com.bjut.s14024205.dao.impl.LiveDaoImpl;
+import com.bjut.s14024205.dao.impl.UserDaoImpl;
+import com.bjut.s14024205.entity.Live;
+import com.bjut.s14024205.entity.User;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserManage extends ActionSupport {
-	
-	//用户名
+
+	// 用户名
 	private String uName;
+	private UserDaoImpl u;
+	private LiveDaoImpl l;
 
 	public String getuName() {
 		return uName;
@@ -14,16 +30,39 @@ public class UserManage extends ActionSupport {
 	public void setuName(String uName) {
 		this.uName = uName;
 	}
-	
-	
-	
-	
-	
-	//获取指定用户的信息
+
+	public UserDaoImpl getU() {
+		return u;
+	}
+
+	public void setU(UserDaoImpl u) {
+		this.u = u;
+	}
+
+	public LiveDaoImpl getL() {
+		return l;
+	}
+
+	public void setL(LiveDaoImpl l) {
+		this.l = l;
+	}
+
+	// 获取指定用户的信息
 	/**
 	 * 通过用户名来查找数据库取得相关用户的信息，以及该用户直播间的信息
+	 * @throws IOException 
 	 */
-	public void getUserInfo() {
-		System.out.println("233");
+	public void getUserInfo() throws IOException {
+		HttpServletRequest req = ServletActionContext.getRequest();
+		HttpSession session = req.getSession();
+		HttpServletResponse resp = ServletActionContext.getResponse();
+		//用户名直接从session里面取就行了
+		this.setuName((String) session.getAttribute("uName"));
+		Live temp_live = l.findByName(this.uName);
+		String output = JSON.toJSONString(temp_live);
+		System.out.println(this.uName);
+		System.out.println(output);
+		resp.getWriter().print(output);
+		
 	}
 }
--- src/main/webapp/js/Manage.js
+++ src/main/webapp/js/Manage.js
@@ -3,8 +3,14 @@
  */
 
 var getUserInfo = function(){
-	var username = $("#userinfo").text();
-	username = username.substring(8);
-	
+	url = "/livingSystem/UserManage_getUserInfo";
+	$.get(url,function(data) {
+		var username = $("#userinfo").text();
+		username = username.substring(8);
+		var info = JSON.parse(data);
+		$("#uName").val(username);
+		$("#sName").val(info.streamName);
+		$("#rName").val(info.roomName);
+	})
 }
 $(getUserInfo);
