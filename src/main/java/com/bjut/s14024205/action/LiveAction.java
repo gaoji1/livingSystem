c8d551a2fbac37aeffd86ce41e824b7e95922edc
example name <974632489@qq.com>
2018-04-04 10:54:34 +0800

    2018年4月4日 10点54分： 	添加了用户开启直播功能，只完成了服务端的一小部分

--- src/main/java/com/bjut/s14024205/action/LiveAction.java
+++ src/main/java/com/bjut/s14024205/action/LiveAction.java
@@ -1,9 +1,23 @@
 package com.bjut.s14024205.action;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
 import com.bjut.s14024205.dao.impl.LiveDaoImpl;
 import com.bjut.s14024205.entity.Live;
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+ * 直播action
+ * 获取直播信息与发布直播都在这里
+ * @author gaoji
+ *
+ */
 public class LiveAction extends ActionSupport {
 
 	/**
@@ -18,6 +32,29 @@ public class LiveAction extends ActionSupport {
 
 	public void setL(LiveDaoImpl l) {
 		this.l = l;
+	}
+	//发起直播
+	/**
+	 * 负责发起直播的
+	 * 直播间的创建实际上时通过red5的直播应用创建的（收到直播流后就会把open标志打开）
+	 * 这个service的作用是：在前端指导用户一步一步完成推流操作
+	 * 用户完成后点击开始直播的按钮，进入到这个service后，查询当前用户的直播数据，检查open标志是不是打开的，
+	 * 如果是，则说明用户已经开始推流，则返回值告诉用户直播间开启成功，如果open标志依然是关闭的，那用户没有推流，
+	 * 前端需要继续引导用户推流
+	 * @throws IOException 
+	 */
+	public void beginLive() throws IOException {
+		HttpServletRequest req = ServletActionContext.getRequest();
+		HttpSession session = req.getSession();
+		HttpServletResponse resp = ServletActionContext.getResponse();
+		String uName = (String) session.getAttribute("uName");
+		Live live = l.findByName(uName);
+		if(live.getIsOpen() == false) {
+			resp.getWriter().print("false");
+		}else {
+			resp.getWriter().println("success");
+		}
+		return;
 	}
 
 	// 查询直播信息(测试)
