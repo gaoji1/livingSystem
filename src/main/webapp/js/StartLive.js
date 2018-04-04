deccecb291196f9d3a588aa2c9838a92ed8f01ce
example name <974632489@qq.com>
2018-04-04 15:29:10 +0800

    2018年4月4日 15点29分： 	添加了发布直播js，可以与服务器端交互来确认当前用户是否已经按照操作完成推流

--- src/main/webapp/js/StartLive.js
+++ src/main/webapp/js/StartLive.js
@@ -2,6 +2,14 @@
  * 
  */
 function StartLive(){
-	alert(1);
+	var url = "/livingSystem/LiveAction_beginLive";
+	$.get(url,function(data){
+		if(data!="false"){
+			alert("live start");
+			$(location).attr('href', '/livingSystem/index.html');
+		}else{
+			alert("your live broadcast  can not start correctly,please follow the recommend and try again");
+		}
+	})
 }
 $("#start").on("click",StartLive);
