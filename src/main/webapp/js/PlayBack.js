6070fabd2a975a190e96248263b4640329a072f2
example name <974632489@qq.com>
2018-04-06 17:30:51 +0800

    2018年4月6日 17点31分： 		在回放播放页面嵌入播放器 		成功嵌入，可以正常播放回放视频

--- src/main/webapp/js/PlayBack.js
+++ src/main/webapp/js/PlayBack.js
@@ -13,7 +13,7 @@ function getAllPlayBack(){
 				temp_span = $('<span class="li-span"></span>');
 				temp_span.text(videos[e].uName);
 				temp_a = $('<a></a>');
-				temp_a.attr("href","index.html");
+				temp_a.attr("href","PlayBackPlay.html#"+videos[e].fileName);
 				temp_a.text(new Date(videos[e].liveTime).toLocaleDateString());
 				temp_li.append(temp_span);
 				temp_li.append(temp_a);
---
+++ src/main/webapp/js/PlayBackPlay.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+function getStreamName(){
+	var host = window.location.href;
+	var i = 0;
+	while(host.charAt(i) != "#"){
+		i++;
+	}
+	var out = host.substring(i+1);
+	return out;
+}
+var out = getStreamName();
+var videoObject = {
+			container: '#video',//“#”代表容器的ID，“.”或“”代表容器的class
+			variable: 'player',//该属性必需设置，值等于下面的new chplayer()的对象
+			autoplay:true,//自动播放
+//			live:true,
+			video:'rtmp://localhost/myApp/'+out//视频地址
+		};
+var player=new ckplayer(videoObject);
